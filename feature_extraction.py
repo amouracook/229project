cabd257fda933bbe54a5ac0384dd590d06d7cb4e
Davyd Tamrazov <davyd.tamrazov@gmail.com>
2020-10-17 10:17:10 -1000

    Minor updates

--- feature_extraction.py
+++ feature_extraction.py
@@ -17,4 +17,12 @@ df = pd.read_csv('SF_41860_Flat.csv')
 from collections import Counter 
 n = Counter(df['DPEVLOC'])
 
+# Number of valid features
+s = sum([n["'{}'".format(i)] for i in range(1,6)])
+    
+#%% Split into train/dev/test set
+from sklearn.model_selection import train_test_split
 
+# X = 
+y = df['DPEVLOC']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
