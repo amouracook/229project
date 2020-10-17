9fd10a042c1bf983afea1e2eff32e4dc19463560
Davyd Tamrazov <davyd.tamrazov@gmail.com>
2020-10-17 09:54:17 -1000

    Feature extraction code.

---
+++ feature_extraction.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 09:42:08 2020
+
+@author: tamrazovd
+"""
+
+import pandas as pd 
+import numpy as np
+import statsmodels.formula.api as sm
+
+df = pd.read_csv('SF_41860_Flat.csv')
+
+
+#%%
+print(np.unique(df['DPEVLOC']))
+
+
