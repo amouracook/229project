29ca79d6c4bce5dd8d7714bc2e1cabcf05fdd7ae
Aaron Appelle <aaron.appelle@gmail.com>
2020-11-07 13:26:37 -0800

    Update linreg.py

--- linreg.py
+++ linreg.py
@@ -13,7 +13,8 @@ from sklearn.linear_model import LinearRegression # estimator
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import balanced_accuracy_score, accuracy_score
 from sklearn.metrics import confusion_matrix
 
 # X_train = np.load('Data Label Encoded/X_train.npy')
@@ -36,9 +37,9 @@ features = X_train.columns
 # Algin indices
 y_train = y_train.set_index(X_train.index)
 
-#%% Feature Selection
+#%% Feature Selection - Linear Regression
 estimator = LinearRegression()
-n_features = 30
+n_features = 15
 selector = RFE(estimator, n_features_to_select=n_features, step=1, verbose=0)
 selector = selector.fit(X_train, y_train)
 selected_features = np.take(features, np.where(selector.support_)[0])
@@ -46,7 +47,7 @@ print(selected_features)
 print(selector.score(X_train,y_train))
 print(selector.score(X_val,y_val))
 
-#%% Linreg Using statsmodels
+# Linear Regression Using statsmodels
 X = sm.add_constant(X_train[selected_features])
 X = X_train[selected_features]
 model = sm.OLS(y_train,X)
@@ -54,25 +55,34 @@ results = model.fit()
 print(results.summary())
 qq = sm.qqplot(results.resid,line="s",markersize=3)
 plt.tight_layout()
-
 ypred = model.predict(X.T) # something wrong
 # print(accuracy_score(y_val, ypred))
 # print(confusion_matrix(y_val, ypred))
 
-#%%
-# Linreg Using sklearn
-clf = LinearRegression()
-clf.fit(X, y_train)
+# # Linreg Using sklearn
+# clf = LinearRegression()
+# clf.fit(X, y_train)
+# print(clf.score(X_val[selected_features], y_val))
+# print(accuracy_score(y_val, clf.predict(X_val[selected_features])))
+# print(confusion_matrix(y_val , clf.predict(X_val[selected_features])))
+
+# # Polynomial features ?
+# from sklearn.preprocessing import PolynomialFeatures
+# polynomial_features= PolynomialFeatures(degree=3)
+# Xp = polynomial_features.fit_transform(X)
+# model = sm.OLS(y_train,Xp)
+# results = model.fit()
+# print(results.summary())
+
+#%% Feature Selection - Ridge Regression
+estimator = RidgeClassifier(class_weight='balanced')
+n_features = 5
+selector = RFE(estimator, n_features_to_select=n_features, step=1, verbose=0)
+selector = selector.fit(X_train, y_train)
+selected_features = np.take(features, np.where(selector.support_)[0])
+clf = RidgeClassifier(class_weight='balanced')
+clf.fit(X_train[selected_features], y_train)
+
 print(clf.score(X_val[selected_features], y_val))
-print(accuracy_score(y_val, clf.predict(X_val[selected_features])))
+print(balanced_accuracy_score(y_val, clf.predict(X_val[selected_features])))
 print(confusion_matrix(y_val , clf.predict(X_val[selected_features])))
-
-#%%
-
-# Polynomial features
-from sklearn.preprocessing import PolynomialFeatures
-polynomial_features= PolynomialFeatures(degree=3)
-Xp = polynomial_features.fit_transform(X)
-model = sm.OLS(y_train,Xp)
-results = model.fit()
-print(results.summary())
